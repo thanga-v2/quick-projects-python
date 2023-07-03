6989e00ce437666815c1eeb9bbc36c2b87dda948
thangasblockchain <thangarajm@icloud.com>
2023-07-03 14:53:56 +0530

    string manipulation

---
+++ l-1.py
@@ -0,0 +1,28 @@
+print("hello! testing python")
+
+message = "hello this is my welcome message"
+print(message)
+
+message = "changing my message"
+print(message)
+
+# finding error and traceback
+# variable name can't start with underscore/number
+
+# message_1="okay"
+# 1_message="test"
+# _message="new test"
+
+mysupermessage = "hellp students this is my new super message"
+print(mysupermessage)
+
+# assigning
+userAge = 19
+print("user age -",userAge)
+
+a=5
+b=10
+# a=b
+b = a
+print("a is - ",a)
+print("b is -",b)
---
+++ l-2 strings.py
@@ -0,0 +1,55 @@
+messgae="this is an experiment of string datatypes in pyhton"
+
+print("actual message - ",messgae)
+print("modified -",messgae.title())
+print("hashed -",messgae.__hash__())
+
+message = "test the new hashed built in variable"
+hashed_message = message.__hash__()
+print(hashed_message)
+print(hashed_message.__hash__())
+print(hashed_message.__hash__())
+
+
+# what is 'f' strings in python?
+# 'f' means format - python formats the string by placing the variables in braces {}
+
+car_make = "BMW"
+car_model = "5er 2018"
+car = f"{car_make} and {car_model}"
+print(car)
+my_message = f"thanga loves to drive {car.upper()}. but he can't"
+print(my_message)
+
+
+# Understanding tabs and white spaces
+
+print("this is a \t in my message")
+message="testing the tab"
+inputsample="hope you got the sample"
+letstab=f"incl tab {message}\t{inputsample}"
+print("tab with f string",letstab)
+# print("tab without f string - ",{messgae} \t {inputsample})
+print("values with \t tab, \n one \n two \n three")
+print("python")
+print("python ")
+
+# r strip will remove the extra white spaces of a string in the right side.
+myfavlanguage = 'python '
+mysecfavlanguage = ' python'
+print(myfavlanguage)
+print(myfavlanguage.rstrip())
+print(mysecfavlanguage.lstrip())
+
+# remove_prefix
+# this will remove the prefix in an URL
+facebookurl = "https://www.facebook.com"
+print(facebookurl.removeprefix("https://"))
+
+
+mymessage = "this is the best course to read about python's functionality"
+mynewmessage = "this is the best course to read about python\"s functionality"
+mysecnewmessage = 'this is the best course to read about python\'s functionality'
+print(mymessage)
+print(mynewmessage)
+print(mysecnewmessage)
